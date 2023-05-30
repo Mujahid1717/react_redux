861c32f86d317d92c4d65c7ea9f3bd77ccda0582
Mujahid1717 <mujahidakhtar2003@gmail.com>
2023-05-30 15:59:40 +0500

    handling style in pro making file config and handle it

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View,TouchableOpacity,TextInput } from 'react-native'
 import {React} from 'react';
 import { increment,decrement,changeValue,changeTheme } from '../../src/redux/action';
 import { useSelector,useDispatch } from 'react-redux';
+import { colors } from '../config/colors'
 
 const Home = (props) => {
 
@@ -11,7 +12,8 @@ const Home = (props) => {
   const theme = useSelector((state)=>state)
   const selectedTheme = theme.ThemeReducer
 
-  // console.log('theme value', selectedTheme)
+  
+  const _styles = styles(colors(selectedTheme));
 
   const onAdd = ()=>{
     dispatch((increment(number)))
@@ -22,23 +24,23 @@ const Home = (props) => {
     }
  
   return (
-    <View style={styles.main(selectedTheme)}>
+    <View style={_styles.main}>
       
-      <Text style={styles.numberStyle(selectedTheme)}>{number}</Text>
-      <View style={styles.container}>
-        <TouchableOpacity style = {styles.ButtonStyle(selectedTheme)} onPress={onAdd}>
-          <Text style={styles.btnText(selectedTheme)}>Increase</Text>
+      <Text style={_styles.numberStyle}>{number}</Text>
+      <View style={_styles.container}>
+        <TouchableOpacity style = {_styles.ButtonStyle} onPress={onAdd}>
+          <Text style={_styles.btnText}>Increase</Text>
          </TouchableOpacity>
-        <TouchableOpacity style = {styles.ButtonStyle(selectedTheme)} onPress={onDec}>
-          <Text style={styles.btnText(selectedTheme)}>Decrease</Text>
+        <TouchableOpacity style = {_styles.ButtonStyle} onPress={onDec}>
+          <Text style={_styles.btnText}>Decrease</Text>
         </TouchableOpacity>
       </View>
       <TextInput placeholder='Enter Number'
-          style = {styles.inputField(selectedTheme)}
+          style = {_styles.inputField}
           value = {change}
           onChangeText={text=>dispatch({type:'CHANGE', payload: text })}
       /> 
-      <TouchableOpacity style = {styles.btnTheme(selectedTheme)}
+      <TouchableOpacity style = {_styles.btnTheme}
         onPress={()=> {
           if(selectedTheme == false){
             dispatch(changeTheme(true))
@@ -47,75 +49,61 @@ const Home = (props) => {
           }
         }}   
       >
-        <Text style={styles.btnText(selectedTheme)}>Change Theme</Text>
+        <Text style={_styles.btnText}>Change Theme</Text>
         </TouchableOpacity>
         </View>
     
   );
 };
-const styles = StyleSheet.create({
-
-  main :(main) =>{
-    return{
+const styles = (theme) => StyleSheet.create({
+  main :{
       flex:1,
       justifyContent:'center',
       alignItems:'center',
-      backgroundColor:main?'white':'black',
+      backgroundColor: theme.primary,
       width:400,
-      }
     },
       container:{
         flexDirection:"row",
-        justifyContent:'space-between',
-        
+        justifyContent:'space-between', 
       },
-      numberStyle:(numberStyle)=>{
-        return{
+      numberStyle: {
         fontSize:40,
         fontWeight:'bold',
         marginBottom:20,
-        color:numberStyle?"black":'white'
-        }
+        color: theme.color
       },
-      btnText:(btnText)=>{
-        return{
-          color:btnText?'white':'black',
+      btnText: {
+          color: theme.text,
           fontSize:20,
           fontWeight:'600'
-        }
       },
-      ButtonStyle:(ButtonStyle)=>{
-        return{
-        backgroundColor:ButtonStyle?'black':'white',
+      ButtonStyle: {
+        backgroundColor: theme.buttonBackground,
         width:'35%',
         padding:15,
         marginHorizontal:20,
         borderRadius:20,
         alignItems:"center",
-        }  
       },
-      inputField:(inputField)=>{
-        return{
+      inputField: {
           borderWidth:1,
-          borderColor:inputField?"black":'white',
-          color:inputField?"black":'white',
+          borderColor: theme.borderColor,
+          color: theme.color,
           padding:20,
           marginTop:20,
           borderRadius:20,
           fontSize:20, 
           width:150
-        }
       },
-      btnTheme:(btnTheme)=>{
-        return{
-        backgroundColor:btnTheme?'black':'white',
+      btnTheme: {
+        backgroundColor:theme.buttonBackground,
         width:'60%',
         padding:15,
         marginTop:40,
         marginHorizontal:20,
         borderRadius:20,
         alignItems:"center",
-        }  
       },
 });
 export default Home;
